91964c461319f851f4e01d875520ec83e037f73f
A tech group <77225190+hacke777@users.noreply.github.com>
2021-07-20 13:41:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -27,11 +27,11 @@ This is a Telegram Rename Bot!
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
    
-<b>👉 Contact help :</b> 👉 <a href="@brtoficcal">Diploy</a>
+<b>👉 Contact help :</b> 👉 <a href="@shakthi963">Diploy</a>
 
 --------
 
-Support Group : @brtoficcal """
+Support Group : @shakthi963 """
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
